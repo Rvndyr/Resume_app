dc9d35664b9cf9d70799fbb5be408216c895a635
Randy Rodriguez <Randyr2125@msn.com>
2021-06-30 13:04:47 -0400

    fixed the show route

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -4,10 +4,10 @@ class StudentController < ApplicationController
     render json: students
   end
 
-  # def show
-  #   student = Student.find_by(id: params [:id])
-  #   render json: student.as_json
-  # end
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
 
   # def create
   #   student = Student.new(
